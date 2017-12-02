ba55b3102748ac3cbe56a5f06548ea48d8f41010
Erick A. Montañez <codeams@gmail.com>
2017-12-01 22:12:31 -0600

    Extract projects list to a separate file

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -9,36 +9,12 @@
 
 <script>
 import Project from '@/components/Project'
+import projects from '@/data/projects'
 
 export default {
   data () {
     return {
-      projects: [
-        {
-          id: 1,
-          name: 'Warriors Lighthouse',
-          role: 'Vue development, UI design.',
-          previewUrl: 'http://assets.app/warriors-lighthouse.png'
-        },
-        {
-          id: 2,
-          name: 'Semple',
-          role: 'iOS development, API design.',
-          previewUrl: 'http://assets.app/semple.png'
-        },
-        {
-          id: 3,
-          name: 'Kiiwik Landing',
-          role: 'Front-end development.',
-          previewUrl: 'http://assets.app/kiiwik-landing.png'
-        },
-        {
-          id: 4,
-          name: 'Congress',
-          role: 'Vue development, UI & interaction design.',
-          previewUrl: 'http://assets.app/congress.png'
-        }
-      ]
+      projects
     }
   },
 
