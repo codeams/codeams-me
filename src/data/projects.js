620b1ccb5a2448e8cb79a836b743b24730008728
Erick A. Montañez <codeams@gmail.com>
2017-12-02 02:18:42 -0600

    Refactor projects data definition

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,29 +1,31 @@
 
 import Project from '@/models/Project'
 
-export default [
-  new Project({
+let projects = [
+  {
     id: 1,
     name: 'Warriors Lighthouse',
     role: 'Vue development, UI design.',
     previewUrl: 'http://assets.app/warriors-lighthouse.png'
-  }),
-  new Project({
+  },
+  {
     id: 2,
     name: 'Semple',
     role: 'iOS development, API design.',
     previewUrl: 'http://assets.app/semple.png'
-  }),
-  new Project({
+  },
+  {
     id: 3,
     name: 'Kiiwik Landing',
     role: 'Front-end development.',
     previewUrl: 'http://assets.app/kiiwik-landing.png'
-  }),
-  new Project({
+  },
+  {
     id: 4,
     name: 'Congress',
     role: 'Vue development, UI & interaction design.',
     previewUrl: 'http://assets.app/congress.png'
-  })
+  }
 ]
+
+export default projects.map(x => new Project(x))
