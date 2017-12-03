658d693bb40a330afc83227f56bb0e77f4cadf90
Erick A. Montañez <codeams@gmail.com>
2017-12-02 21:11:36 -0600

    Fix project capture ratio keeping feature

--- src/components/Project-capture.vue
+++ src/components/Project-capture.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class='project-capture'
-    :style='{ "background-image": `url(${ url })` }'>
+  <div class='project-capture'>
+    <div class='image'
+      :style='{ "background-image": `url(${ url })` }'>
+    </div>
   </div>
 </template>
 
@@ -12,15 +14,24 @@ export default {
 
 <style lang='scss'>
 .project-capture {
+  position: relative;
   width: 100%;
   height: 0px;
   padding-top: calc(100% * 0.865);
 
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: 90% 90%;
   background-color: get-color(teen-palenight);
-
   border-radius: 4px;
+
+  .image {
+    position: absolute;
+    top: 5%;
+    left: 5%;
+    width: 90%;
+    height: 90%;
+
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: contain;
+  }
 }
 </style>
