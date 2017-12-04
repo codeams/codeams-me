066c7b1a0d089436cf984fa70258e82648ed8e25
Erick A. Montañez <codeams@gmail.com>
2017-12-03 21:56:06 -0600

    Fix statement role points in technical card

--- src/components/Project-preview.vue
+++ src/components/Project-preview.vue
@@ -7,7 +7,7 @@
     <div class='label'>
       <span class='name'>{{ instance.name }}</span>
       <span class='dash'> — </span>
-      <span class='role'>{{ instance.role }}</span>
+      <span class='role'>{{ instance.role }}.</span>
     </div>
   </div>
 </template>
--- src/data/projects.js
+++ src/data/projects.js
@@ -6,7 +6,7 @@ let projects = [
     id: 'warriors-lighthouse',
     type: 'Personal project',
     name: 'Warriors Lighthouse',
-    role: 'Vue development, UI design.',
+    role: 'Vue development, UI design',
     story: 'I did SPA development using Vue and UI design using Adobe Experience Design. The main idea of the project was to increase the quality of Warriors company processes and reduce the number of human errors.',
     collabs: ['isisnaomi'],
     startDate: 'March 2017',
@@ -26,20 +26,20 @@ let projects = [
     id: 'semple',
     type: 'Yellowme Project',
     name: 'Semple',
-    role: 'iOS development, API design.',
+    role: 'iOS development, API design',
     story: '',
     captures: ['http://assets.app/semple.png']
   },
   {
     id: 'kiiwik-landing',
     name: 'Kiiwik Landing',
-    role: 'Front-end development.',
+    role: 'Front-end development',
     captures: ['http://assets.app/kiiwik-landing.png']
   },
   {
     id: 'congress',
     name: 'Congress',
-    role: 'Vue development, UI & interaction design.',
+    role: 'Vue development, UI & interaction design',
     captures: ['http://assets.app/congress.png']
   }
 ]
