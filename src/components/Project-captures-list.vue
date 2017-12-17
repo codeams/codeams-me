8e0e7fc048a1860c3b9bf4463be616e1abd8a98e
Erick A. Montañez <codeams@gmail.com>
2017-12-17 11:35:11 -0600

    Refactor project captures list hide scrollbars implementation

--- src/components/Project-captures-list.vue
+++ src/components/Project-captures-list.vue
@@ -29,16 +29,14 @@ export default {
 
 <style lang='scss'>
 .project-captures-list {
+  @extend .no-scrollbars;
+  
   width: 100%;
   height: 100%;
   padding-top: 140px;
   padding-bottom: 60px;
   overflow-x: hidden;
   overflow-y: scroll;
-
-  &::-webkit-scrollbar {
-    display: none; 
-  }
 }
 
 .capture-container {
